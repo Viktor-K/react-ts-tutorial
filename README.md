15c5e1c6b0a69b4b6e8cbbc2ae34a8accc133c71
vcaprio <vcaprio@byte-code.com>
2018-03-15 10:02:43 +0100

    SharkDiscovery use random Proxy servers

--- README.md
+++ README.md
@@ -1,8 +1,18 @@
 # react-ts-tutorial
 Simple tutorial for React + Typescript
+======= 
+#Task Memorandum : 
+
+[x] Init application with create-react-app for typescript
+[x] Add less preprocess
+[x] Add libs with properly @types
+[x] Add React-Bootstrap (v3.0) libs
+[] Add Redux library
+[] Add React Material UI Components
+
 =======
 This project was bootstrapped with [Create React App](https://github.com/facebookincubator/create-react-app).
-
+  
 Below you will find some information on how to perform common tasks.<br>
 You can find the most recent version of this guide [here](https://github.com/facebookincubator/create-react-app/blob/master/packages/react-scripts/template/README.md).
 
--- src/components/shark-discover/SharkDiscover.tsx
+++ src/components/shark-discover/SharkDiscover.tsx
@@ -1,21 +1,17 @@
 import * as React from 'react';
 import * as _ from 'lodash';
 import './SharkDiscover.css';
-let Glyphicon = require('react-bootstrap/lib/Glyphicon');
-const axios = require('axios');
 
-// const XCORS_HEADERS = {
-//     headers: {
-//         'Access-Control-Allow-Origin': '*',
-//         'Access-Control-Allow-Methods': 'GET',
-//         'Access-Control-Allow-Credentials': true,
-//         'Access-Control-Allow-Headers': 'Content-Type'
-//     }
-// };
+const axios = require('axios');
+const Glyphicon = require('react-bootstrap/lib/Glyphicon');
+const ENABLE_DEBUG = false;
 const DEFAULT_SHARK_IMG = 'https://cdn.onlinewebfonts.com/svg/img_74287.png';
+const PROXY_SERVERS = ['http://cors-anywhere.herokuapp.com', 'https://crossorigin.me'];
 
 function printLog(msg: string) {
-    console.log(`DEBUG : ${msg}`);
+    if (ENABLE_DEBUG) {
+        console.log(`DEBUG : ${msg}`);
+    }
 }
 
 type SharksProps = {
@@ -57,13 +53,11 @@ class SharksDiscover extends React.Component<SharksProps, SharksState> {
         printLog(`Fetch Shark image perform search : ${JSON.stringify(this.state)}`);
 
         if (typeof sharkName === 'string') {
-            let targetUrl = `http://api.qwant.com/api/search/images?count=100&offset=1&q=${sharkName}%arkive.org`;
+            const targetUrl = `http://api.qwant.com/api/search/images?count=100&offset=1&q=${sharkName}%arkive.org`;
+            const requestUrl = `${PROXY_SERVERS[_.random(0, 1)]}/${targetUrl}`;
 
-            // const proxyUrl = `http://cors-anywhere.herokuapp.com/${targetUrl}`;
-            const proxyUrl = `https://crossorigin.me/${targetUrl}`;
-
-            printLog(`perform search : ${proxyUrl}`);
-            axios.get(`${proxyUrl}`)
+            printLog(`perform search : ${requestUrl}`);
+            axios.get(`${requestUrl}`)
                 .then(function (response: object) {
                     let results: object[] = _.get(response, 'data.data.result.items', []);
                     if (results.length > 0) {
@@ -99,19 +93,17 @@ class SharksDiscover extends React.Component<SharksProps, SharksState> {
 
     renderSharkImage(sharkImgUrl: string | undefined) {
         if (typeof sharkImgUrl === 'string') {
-            let imgTag = (sharkImgUrl === DEFAULT_SHARK_IMG)
-                ? (
-                    <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'space-between'}}>
-                        <Glyphicon style={{margin: '1em', color: 'red', fontSize: '2em'}} glyph="eye-close">                        
-                            <span style={{marginLeft: '10px'}}>Image not available</span>
-                        </Glyphicon>
-                        <img style={{boxShadow: 'none'}} src={sharkImgUrl} />                    
-                    </div>)
-                :  <img src={sharkImgUrl} />;
-
             return (
-                <div className="img-container">                    
-                    {imgTag}
+                <div className="img-container">
+                    {(sharkImgUrl === DEFAULT_SHARK_IMG)
+                        ? (
+                            <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}>
+                                <Glyphicon style={{ margin: '1em', color: 'red', fontSize: '2em' }} glyph="eye-close">
+                                    <span style={{ marginLeft: '10px' }}>Image not available</span>
+                                </Glyphicon>
+                                <img style={{ boxShadow: 'none' }} src={sharkImgUrl} />
+                            </div>)
+                        : <img src={sharkImgUrl} />}
                 </div>
             );
         } else {
